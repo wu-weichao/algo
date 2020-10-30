04a221d7238e129ae935a07130e48e55043648f8
吴伟超 <wuweichao@dobest.com>
2020-10-30 16:56:23 +0800

    桶排序 计数排序 基数排序

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -39,3 +39,24 @@ func TestQuickSort(t *testing.T) {
 	QuickSort(data)
 	fmt.Printf("new array: %v \r\n", data)
 }
+
+func TestBucketSort(t *testing.T) {
+	data := []int{2, 5, 9, 10, 9, 3, 4, 2, 5, 3}
+	fmt.Printf("old array: %v \r\n", data)
+	BucketSort(data)
+	fmt.Printf("new array: %v \r\n", data)
+}
+
+func TestCountingSort(t *testing.T) {
+	data := []int{2, 5, 9, 10, 9, 3, 4, 2, 5, 3}
+	fmt.Printf("old array: %v \r\n", data)
+	CountingSort(data)
+	fmt.Printf("new array: %v \r\n", data)
+}
+
+func TestRadixSort(t *testing.T) {
+	data := []int{87, 98, 156, 153, 57, 58, 93, 18, 5, 23}
+	fmt.Printf("old array: %v \r\n", data)
+	RadixSort(data)
+	fmt.Printf("new array: %v \r\n", data)
+}
